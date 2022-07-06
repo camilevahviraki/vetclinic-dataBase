d621e567cea2c72b7b47d169dea93b4d8d810317
camilevahviraki <camilevahviraki@gmail.com>
2022-07-06 14:43:33 +0200

    Add queries

--- queries.sql
+++ queries.sql
@@ -2,18 +2,22 @@
 
 SELECT * from animals WHERE name LIKE '%mon';
 
-SELECT * FROM animals WHERE To_date BETWEEN '2016-01-01' AND '2019-01-01';
+SELECT * FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-01-01';
 
-SELECT * FROM animals WHERE neutered = 1 AND escape_attempts < 3;
+SELECT * FROM animals WHERE neutered = 'true' AND escape_attempts < 3;
 
 SELECT date_of_birth FROM animals WHERE name = 'Agumon' OR name = 'Pikachu';
 
 SELECT name, escape_attempts FROM animals WHERE weight_kg >= 10.5;
 
-SELECT * FROM animals WHERE neutered = 1;
+SELECT * FROM animals WHERE neutered = 'true';
 
 SELECT * FROM animals WHERE name != 'Gabumon';
 
 SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
 
+UPDATE animals SET species = 'unspecified';
 
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+
+UPDATE animals SET species = 'pokemon' WHERE species = 'unspecified';
--- schema.sql
+++ schema.sql
@@ -1,11 +1,5 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    id int,
-    name varchar(100),
-    date_of_birth date,
-    escape_attempts int,
-    neutered boolean,
-    weight_kg decimal,
-    PRIMARY KEY (id)
-);
+CREATE TABLE animals (id int, name varchar(100), date_of_birth date, escape_attempts int, neutered boolean, weight_kg decimal, PRIMARY KEY (id));
+
+ALTER TABLE animals ADD species varchar(200);
