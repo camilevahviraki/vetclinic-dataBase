f3f82829a768b0c6de3f537f3b718262a282adc1
camilevahviraki <camilevahviraki@gmail.com>
2022-07-07 13:01:41 +0200

    Update queries and create new tables

--- schema.sql
+++ schema.sql
@@ -3,3 +3,9 @@
 CREATE TABLE animals (id int, name varchar(100), date_of_birth date, escape_attempts int, neutered boolean, weight_kg decimal, PRIMARY KEY (id));
 
 ALTER TABLE animals ADD species varchar(200);
+ALTER TABLE animals DROP COLUMN id;
+ALTER TABLE animals ADD id SERIAL PRIMARY KEY;
+
+CREATE TABLE owners (id SERIAL PRIMARY KEY, full_name varchar(100), age int);
+
+CREATE TABLE species (id SERIAL PRIMARY KEY, name varchar(100));
